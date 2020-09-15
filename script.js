535439e333fdc2d8f2e6d48268523ec0401d11d2
Bridget Shannon <bridgetrshannon@gmail.com>
2020-09-14 20:35:16 -0400

    placed the function names for the steps open page, start quiz, show timer, next question, and check answer

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log(questions);
+// Hook question container
+var displayQuestionsEl = document.querySelector(".display-questions")
+// Hook timer element
+var timeEl = document.querySelector(".timer")
+
+
+
+function openingPage()
+
+
+
+
+function startQuiz()
+
+
+
+
+function showTimer()
+
+
+
+function nextQuestion()
+
+
+
+function checkAnswer()
